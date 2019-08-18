8c6e72f8f549edb2d3f216d8199ef4f8038c6d75
Dilane237 <momodilane7@gmail.com>
2019-08-18 10:14:33 -0500

    introduction to looping

---
+++ src/main.kt
@@ -0,0 +1,10 @@
+
+
+fun main(){
+    val interestingThings = arrayOf("Kotlin","Programming","Comic Books")
+    println(interestingThings.size)
+
+    for(item in interestingThings){
+        println(item)
+    }
+}
