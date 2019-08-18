684dd9f6f6ca95af2e9f0abfc38a822dfa29f901
Dilane237 <momodilane7@gmail.com>
2019-08-18 17:06:12 -0500

    Accessing constructor in a method

--- src/Person.kt
+++ src/Person.kt
@@ -1,2 +1,9 @@
-class Person {
+class Person( val firstName:String, val lastName:String){
+//    constructor() : this()
+
+    constructor(): this("Dilane","Azangue"){
+        println(this.firstName)
+        println(this.lastName)
+    }
+
 }
--- src/main.kt
+++ src/main.kt
@@ -1,14 +1,7 @@
-fun sayHello(greetings:String, vararg itemToGreet:String){
-    itemToGreet.forEach { element ->
-        println("$greetings $element")
-    }
-}
+
 
 fun main(){
-    val personToGreet = arrayOf("Dilane","Mesmer","Magloire","Tianou")
 
-
-        sayHello(itemToGreet = *personToGreet,greetings = "Hello")
-
+    val person = Person()
 
 }
