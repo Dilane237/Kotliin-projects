039c3853d9c3db309ef9e4d7648674ecf0a1418b
Dilane237 <momodilane7@gmail.com>
2019-08-18 23:01:30 -0500

    introduction to inheritance

--- src/Person.kt
+++
@@ -1,9 +0,0 @@
-class Person( val firstName:String, val lastName:String){
-//    constructor() : this()
-
-    constructor(): this("Dilane","Azangue"){
-        println(this.firstName)
-        println(this.lastName)
-    }
-
-}
