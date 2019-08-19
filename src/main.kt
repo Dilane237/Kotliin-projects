e9cac8953f76c5a21d923dc6bde729786845fea6
Dilane237 <momodilane7@gmail.com>
2019-08-19 09:20:33 -0500

    superclass displays value initialized in the subclass secondary constructor

--- src/main.kt
+++ src/main.kt
@@ -2,6 +2,38 @@
 
 fun main(){
 
-    val person = Person()
+    val authLog = AuthLog("Bad Password")
 
 }
+
+open class Log{
+    var data: String = ""
+    var numberOfTimes = 0
+
+    //define secondary constructor of class Log
+    constructor(_data: String){
+        data = _data
+    }
+
+    //secondary constructor with different parameters
+    constructor(_data: String, _numberOfTimes: Int){
+        data = _data
+        numberOfTimes = _numberOfTimes
+
+        //display values collected from the derived class
+        println("$data:  $numberOfTimes times")
+    }
+}
+
+class AuthLog: Log{
+
+    //here we initialize the constructor because the primary constrcutor has not been initialized
+    constructor(_data: String): this(_data,10) //accessing _numberOfTimes using this
+
+    //here we call the superclass base constructor using super
+    constructor(_data: String,_numberOfTimes: Int): super(_data,_numberOfTimes){
+
+    }
+
+}
+
